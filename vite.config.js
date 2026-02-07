1f2defd248c268d35363fd030565e7f40a8b76b2
tatahosny <tatahosny208@gmail.com>
2026-02-07 21:14:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          'vue-router': ['vue-router'],
+          pinia: ['pinia']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 })
