6879f015566f34f211fa0f072313a1c3c2af238d
tatahosny <tatahosny208@gmail.com>
2026-02-07 21:27:56 +0200

    Integrate vueDevTools and modify path resolution
    Added vueDevTools plugin and updated path resolution.

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vue: ['vue'],
-          'vue-router': ['vue-router'],
-          pinia: ['pinia']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    host: true
-  }
 })
